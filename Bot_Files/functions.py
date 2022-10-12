bf1efc2273d973ce4a7621e86fb77fc7ec7ce1ca
Bishal Sahoo <thecodebloodedguy@protonmail.ch>
2022-10-12 12:01:48 +0530

    conversion functions

---
+++ Bot_Files/functions.py
@@ -0,0 +1,249 @@
+
+def q1(n):
+ 
+ 
+      if n=="bio":
+          return 1
+      elif n=="maths":
+          return 2
+      elif n=="commerce":
+          return 3
+      elif n=="arts":
+          return 4
+      else:
+        return 0
+
+def q2(n):
+ 
+ 
+      if n=="male":
+          return 1
+      elif n=="female":
+          return 2
+      else:
+        return 3
+
+def q3(n):
+  if(n>=0 and n<=10):
+    return n
+
+def q4(n):
+  if(n>=0 and n<=10):
+    return n
+
+def q5(n):
+  if(n>=0 and n<=550):
+    return n
+
+
+def q6(n):
+ 
+ 
+      if n=="yes":
+          return 1
+      elif n=="no":
+          return 0
+
+def q7(n):
+  if(n>=1 and n<=5):
+    return n
+  else:
+    return 0
+
+
+def q8(n):
+ 
+ 
+      if n=="yes":
+          return 1
+      elif n=="no":
+          return 0
+
+def q9(n):
+      if n=="music":
+          return 1
+      elif n=="theatre":
+          return 2
+      elif n=="literature":
+          return 3
+      elif n=="fineart":
+          return 4
+      elif n=="tourism":
+          return 5    
+      else:
+        return 0
+
+def q10(n):
+  if(n>=0 and n<=10):
+    return n
+
+def q11(n):
+  if(n>=0 and n<=10):
+    return n
+
+
+def q12(n):
+ 
+ 
+      if n=="yes":
+          return 1
+      elif n=="no":
+          return 0
+
+def q13(n):
+  if(n>=1 and n<=10):
+    return n
+      
+
+
+def q14(n):
+  if(n>=1 and n<=10):
+    return n
+def q15(n):
+ 
+ 
+      if n=="yes":
+          return 1
+      elif n=="no":
+          return 0
+
+def q16(n):
+  if(n>=1 and n<=10):
+    return n
+  else:
+    return 0
+
+      
+     
+
+def q17(n):
+ 
+ 
+      if n=="yes":
+          return 1
+      elif n=="no":
+          return 0
+
+def q18(n):
+  if n=="software":
+    return 1
+  elif n=="hardware":
+    return 2
+
+
+
+def q19(n):
+  if(n>=6 and n<=10):
+    return n
+  else:
+    return 0
+
+
+
+def q20(n):
+  if(n>=4 and n<=10):
+    return n
+  else:
+    return 0
+
+def q21(n):
+ 
+ 
+      if n=="yes":
+          return 1
+      elif n=="no":
+          return 0
+
+def q22(n):
+      if n=="nutritionist":
+          return 1
+      elif n=="medical":
+          return 2
+      else:    
+         return 0
+
+def q23(n):
+      if n=="veterinary":
+          return 1
+      elif n=="pediatrician":
+          return 2
+      elif n=="physician":
+          return 3
+      elif n=="dental":
+          return 4
+      elif n=="orthopaedic":
+          return 5    
+      else:
+        return 0
+
+def q24(n):
+ 
+ 
+      if n=="yes":
+          return 1
+      elif n=="no":
+          return 0
+
+def q25(n):
+      if n=="industrial":
+          return 1
+      elif n=="agriculture":
+          return 2
+      elif n=="enterprise":
+          return 3
+      elif n=="construction":
+          return 4 
+      else:
+        return 0
+     
+
+def q26(n):
+  if(n>=0 and n<=10):
+    return n
+  else:
+    return 0  
+
+
+def q27(n):
+  if(n>=1 and n<=10):
+    return n
+  else:
+    return 0  
+      
+
+def q28(n):
+  if(n>=1 and n<=10):
+    return n
+  else:
+    return 0
+
+def output(n):
+      if n== 1:
+          return "Sports Science"
+      elif n== 2:
+          return "Theatre & Tourism"
+      elif n== 3:
+          return "Teaching"
+      elif n== 4:
+          return "Marketing Management"
+      elif n== 5:
+          return "Chemical Engineering"
+      elif n== 6 :
+          return "S/W based engineering"
+      elif n== 7:
+          return "H/W based Engineering"
+      elif n== 8 :
+          return "Medicine and Nutrition"
+      elif n== 9 :
+          return "Business/Industrial Mngmnt."
+      elif n== 10 :
+          return "UPSC/GPSC etc."
+      elif n== 11 :
+          return "CA/CS/Banking"
+      elif n== 12 :
+          return "Graphics & Designing"
+      else:
+          return "Arts"    
+
+            
+
